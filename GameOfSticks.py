d088667540c52b902eb282ed37f75a9c97a049a2
Aaron Scruggs <aarondscruggs@gmail.com>
2016-03-07 20:00:23 -0800

    Added docs and refactored main

--- GameOfSticks.py
+++ GameOfSticks.py
@@ -20,8 +20,14 @@ class Player:
 
     def __init__(self, number):
         self.player_number = number
+        self.is_human = True
 
     def make_selection(self, total_sticks):
+        """
+        Requires player to make a valid selection.
+        :param total_sticks: Total remaining sticks.
+        :return: Player choice of how many sticks to remove.
+        """
         choice = " "
         most = min([3, int(total_sticks)])
         while not choice.isnumeric() or int(choice) > most:
@@ -39,27 +45,42 @@ class PlayerAI(Player):
     def __init__(self, number):
         super().__init__(number)
         self.used_plays = []
+        self.is_human = False
 
     def initialize_hat(self, max_number=100):
-        # Run on initial setup
-        self.hats = {i: [1,2,3] for i in range(3, max_number + 1)}
+        """
+        Creates an initial dictionary of possible plays.
+        :param max_number: Highest number of sticks considered.
+        :return: Initial dictionary.
+        """
+        self.hats = {i: [1, 2, 3] for i in range(3, max_number + 1)}
         self.hats[2] = [1, 2]
         self.hats[1] = [1]
         return self.hats
 
     def make_selection(self, total_sticks):
+        """
+        AI player randomly selects a choice.
+        :param total_sticks: The current number of sticks for current play.
+        :return: Number of sticks to pick up.
+        """
         choice = random.choice(self.hats[total_sticks])
         print("The AI selects {}".format(choice))
         self.used_plays.append((total_sticks, choice))
         return choice
 
     def update_after_result(self, win):
+        """
+        Updates the play dictionary after a game finishes.
+        :param win: Whether or not the AI won the last game.
+        :return: A new hats dictionary and clean used_plays list.
+        """
         if win:
             for plays in self.used_plays:
                 self.hats[plays[0]].append(plays[1])
         else:
             for plays in self.used_plays:
-                if len(self.hats[plays[0]]) > 1:
+                if self.hats[plays[0]].count(plays[1]) > 1:
                     self.hats[plays[0]].remove(plays[1])
         self.used_plays = []
 
@@ -72,7 +93,12 @@ class Game:
         self.players = [self.player1, self.player2]
 
     def turn(self):
-
+        """
+        One turn of gameplay. Both players act and the number of sticks is
+        adjusted appropriately.
+        :return: A tuple of the remaining sticks and either the losing
+        player number or zero.
+        """
         for player in self.players:
             print("\nIt is player {}'s turn".format(player.player_number))
             print("There are {} sticks on the board".format(self.sticks))
@@ -80,11 +106,14 @@ class Game:
             self.sticks -= action
 
             if self.sticks < 1:
-                #print("Player {} has lost".format(player.player_number))
                 return 0, player.player_number
-        return self.sticks, 0  # Better way?
+        return self.sticks, 0
 
     def run_game(self):
+        """
+        The main game function.
+        :return: Player number of losing the losing player.
+        """
         while self.sticks > 0:
             results = self.turn()
             self.sticks = results[0]
@@ -92,10 +121,6 @@ class Game:
         print("Game over")
         return results[1]
 
-
-# Options to play against computer or human
-# Option to play again
-# - must preserve AI brain
 
 if __name__ == '__main__':
 
@@ -108,7 +133,7 @@ if __name__ == '__main__':
 
     starting = int(starting)
 
-    game_mode = '0'
+    game_mode = "0"
 
     while not game_mode.isnumeric() or int(game_mode) not in [1, 2]:
         print("Options:",
@@ -125,27 +150,22 @@ if __name__ == '__main__':
     if game_mode == 1:
         player1 = Player(1)
         player2 = Player(2)
-
-        while new_game:
-            active_game = Game(starting, player1, player2)
-            active_game.run_game()
-            new_game = next_game()
-
     elif game_mode == 2:
         player1 = Player(1)
         player2 = PlayerAI(2)
-        player2.initialize_hat(max_number=10)
+        player2.initialize_hat()
 
-        while new_game:
-            active_game = Game(starting, player1, player2)
-            loser = active_game.run_game()
-            if loser == 1:
-                player2.update_after_result(True)
-            else:
-                player2.update_after_result(False)
-            print(player2.hats)
+    while new_game:
+        active_game = Game(starting, player1, player2)
+        loser = active_game.run_game()
 
-            new_game = next_game()
+        if loser == 1 and not player2.is_human:
+            player2.update_after_result(True)
+        elif loser == 2 and not player2.is_human:
+            player2.update_after_result(False)
 
+        print(player2.hats)
+
+        new_game = next_game()
 
 
