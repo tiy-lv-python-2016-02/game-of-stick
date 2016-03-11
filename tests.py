56d1944a9c6aadee6beecf59b1fa527be91f3620
Kevin Mullen <kevinkozzik@gmail.com>
2016-03-11 14:54:01 -0800

    Updated Tests.py

--- tests.py
+++ tests.py
@@ -1,32 +1,28 @@
 from unittest import TestCase
-from sticks import Hat, HatAIPlayer, Game
+from sticks import HatAIPlayer, Game, Player
 
 
 class HatAIPlayerTests(TestCase):
 
     def setUp(self):
-
-        self.player = HatAiPlayer("Tester")
+        self.sticks = 10
+        self.player = HatAIPlayer("Tester", self.sticks)
 
     def test_init(self):
 
         self.assertEqual(self.player.name, "Tester", msg="Name incorrect")
         self.assertEqual(self.player.win, 0, msg="Win not starting at 0")
         self.assertEqual(self.player.loss, 0, msg="Loss not starting at 0")
-        self.assertEqual(self.player.hats, [], msg="Hats not reset for session")
-
-    def test_take_sticks(self):
-
-        take_sticks = self.player.take_sticks()
-        self.assertTrue(1 <= take_sticks <= 3, msg="Not taking sticks")
 
 
 class GameTests(TestCase):
 
     def setUp(self):
 
+        sticks = 10
         self.player1 = Player("Player 1")
         self.player2 = Player("Player 2")
+        self.test_game = Game(self.player1, self.player2, sticks)
 
     def test_init(self):
 
